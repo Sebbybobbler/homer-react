a9acea65500bc3d8a545d8ad9f1513a1c754423f
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-04 15:52:31 +0100

    added functionality to Column component, auto adds img's via array mapping

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,42 +1,45 @@
 /* eslint-disable jsx-a11y/alt-text */
 import styles from "./Column.module.css";
-import clock from "../../assets/images/clock.gif";
-
-
 
 export default function Column(props) {
-    const imagetype=props.content;
-
+    const imagetype = props.content;
     return (
-        <>
-            <div className={`${styles.column} ${"column1"}`}>
-                <img style={{width:"40%"}}
-                    className={styles.left}
-                    src={imagetype[0]}
-                    
-                />
-                <img
-                    className={styles.right}
-                    src={imagetype[1]}
-                
-                />
-                <img
-                    style={{width:"80%"}}
-                    className={styles.left}
-                    src={imagetype[2]}
-                    
-                />
-                <img
-                    className={styles.right}
-                    src={imagetype[3]}
-                    
-                />
-                <img
-                    className={styles.right}
-                    src={imagetype[4]}
-                    
-                />
-            </div>
-        </>
+        <div>
+            {imagetype.map((image, index) => {
+                return <img src={image} />;
+            })}
+        </div>
     );
+    // return (
+    //     <>
+    //         <div className={`${styles.column} ${"column1"}`}>
+    //             <img style={{width:"40%"}}
+    //                 className={styles.left}
+    //                 src={imagetype[0]}
+
+    //             />
+    //             <img
+    //                 className={styles.right}
+    //                 src={imagetype[1]}
+
+    //             />
+    //             <img
+    //                 style={{width:"80%"}}
+    //                 className={styles.left}
+    //                 src={imagetype[2]}
+
+    //             />
+    //             <img
+    //                 className={styles.right}
+    //                 src={imagetype[3]}
+
+    //             />
+    //             <img
+    //                 className={styles.right}
+    //                 src={imagetype[4]}
+
+    //             />
+    //         </div>
+    //     </>
+    // );
 }
