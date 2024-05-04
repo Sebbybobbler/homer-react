d8e8c9b10def0af6d5e18434e81ac0f6249f3671
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-04 15:46:24 +0100

    added images and passed props down to make adding easier

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,10 +1,42 @@
+/* eslint-disable jsx-a11y/alt-text */
 import styles from "./Column.module.css";
-import clock from "../../assets/images/clock";
+import clock from "../../assets/images/clock.gif";
 
-export default function Column(){
-    return(
-        <div className={styles.column}>
-            <img src={clock} alt="gif of a racing clock"/>
-        </div>
-    )
+
+
+export default function Column(props) {
+    const imagetype=props.content;
+
+    return (
+        <>
+            <div className={`${styles.column} ${"column1"}`}>
+                <img style={{width:"40%"}}
+                    className={styles.left}
+                    src={imagetype[0]}
+                    
+                />
+                <img
+                    className={styles.right}
+                    src={imagetype[1]}
+                
+                />
+                <img
+                    style={{width:"80%"}}
+                    className={styles.left}
+                    src={imagetype[2]}
+                    
+                />
+                <img
+                    className={styles.right}
+                    src={imagetype[3]}
+                    
+                />
+                <img
+                    className={styles.right}
+                    src={imagetype[4]}
+                    
+                />
+            </div>
+        </>
+    );
 }
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,10 +1,40 @@
 import styles from "./MainContent.module.css";
-import Column from "../Column/Column"
-export default function MainContent(){
+import Column from "../Column/Column";
+import clock from "../../assets/images/clock.gif";
 
-    return(
-        <div className={styles.body}>
-            <Column/>
-        </div>
-    )
+const images = {
+    empty: " ",
+    clock: clock,
+    worms: "https://images.squarespace-cdn.com/content/v1/581499b8e58c627afce6221c/1581506894615-92DOP8D8ZXT93BMQFUL4/worm-boyz.gif?format=1000w",
+    toaster: "https://coolgifs.neocities.org/gifs/93.gif",
+    bells: "https://media.tenor.com/pJCgr-KvGW8AAAAj/bells-wedding-bells.gif",
+    mouth: "https://media.baamboozle.com/uploads/images/627098/1644409034_191338_gif-url.gif",
+};
+
+export default function MainContent(props) {
+    return (
+        <>
+            <div className={`${styles.body} ${styles.column}`}>
+                <Column
+                    content={[
+                        images["clock"],
+                        images["worms"],
+                        images["toaster"],
+                        images["bells"],
+                        images["worms"],
+                    ]}
+                />
+            </div>
+            <div className={`${styles.body} ${styles.column}`}>
+                <Column
+                    content={[
+                        images["toaster"],
+                        images["mouth"],
+                        images["clock"],
+                        images["worms"],
+                    ]}
+                />
+            </div>
+        </>
+    );
 }
